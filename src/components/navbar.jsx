6ddde01c8cc0fbc9d4ee9815c9d8e0d8b35e84ad
Sushanth Reddy <sushanthreddy717@gmail.com>
2024-06-16 07:46:56 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     const authStatus = useSelector(store => store.auth.userRole);
     return (
         <nav className="flex justify-between items-center py-5 px-10 border-b border-gray-300">
-            <div className="text-3xl font-extrabold">MIT ADT</div>
+            <div className="text-3xl font-extrabold">Academic Record Storage</div>
             {authStatus === null && <ul className="flex gap-10">
                 <li className="cursor-pointer">Home</li>
                 <li className="cursor-pointer">Feature</li>
